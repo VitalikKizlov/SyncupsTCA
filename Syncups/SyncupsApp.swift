7c155c0a3ca080173a9ca5aed192feebad9a2115
Vitalii Kizlov <vitalii kizlov>
2024-12-01 21:00:31 +0100

    Initial Commit

---
+++ Syncups/SyncupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncupsApp.swift
+//  Syncups
+//
+//  Created by Vitalii Kizlov on 01.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SyncupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
